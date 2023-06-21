eb1e7faa5ef0041d467f17564e9c98300119bad4
Natalie Cooper <ncooper@tcd.ie>
2023-06-21 16:33:30 +0100

    Add fossil/extant trees

--- biogeography/analyses/00-prepare-trees.R
+++ biogeography/analyses/00-prepare-trees.R
@@ -4,16 +4,40 @@
 library(ape)
 
 # Read in full tree
-tree <- read.tree("biogeography/data/pinnipedia_mcc_clean.tre")
+tree <- read.tree("biogeography/raw-data/pinnipedia_mcc_clean.tre")
 
 #-----------------------------------
 # 9 areas tree
 #-----------------------------------
 # Read in 9 areas geographical data
-geo <- read.delim("biogeography/data/pinniped-all-geography_9areas.txt")
+geo1 <- read.delim("biogeography/data/pinniped-all-geography_9areas.txt")
+
+# Drop species not in the geography file
+tree1 <- drop.tip(tree, setdiff(tree$tip.label, geo$X105))
+
+# Save tree
+write.tree(tree1, "biogeography/data/pinniped-tree-all_9areas.tre")
+
+#-----------------------------------
+# 9 areas tree - extant only
+#-----------------------------------
+# Read in 9 areas geographical data
+geo2 <- read.delim("biogeography/data/pinniped-extant-geography_9areas.txt")
 
 # Drop species not in the geography file
 tree2 <- drop.tip(tree, setdiff(tree$tip.label, geo$X105))
 
 # Save tree
-write.tree(tree2, "biogeography/data/pinniped-tree-all_9areas.tre")
+write.tree(tree2, "biogeography/data/pinniped-tree-extant_9areas.tre")
+
+#-----------------------------------
+# 9 areas tree - fossil only
+#-----------------------------------
+# Read in 9 areas geographical data
+geo3 <- read.delim("biogeography/data/pinniped-fossil-geography_9areas.txt")
+
+# Drop species not in the geography file
+tree3 <- drop.tip(tree, setdiff(tree$tip.label, geo$X105))
+
+# Save tree
+write.tree(tree3, "biogeography/data/pinniped-tree-fossil_9areas.tre")
