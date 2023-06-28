dc5945f5c6d79cd4e65f2ea5d2e1c6c70f98f6d1
Natalie Cooper <ncooper@tcd.ie>
2023-06-28 17:41:24 +0100

    Update plotting script

--- biogeography/analyses/06-DEC-DECj-plots.R
+++ biogeography/analyses/06-DEC-DECj-plots.R
@@ -6,14 +6,10 @@
 #------------------------
 library(BioGeoBEARS)
 library(ape)
-
-# Quick AIC function
-AIC <- function(loglik, nparams){
-  2*nparams - 2*loglik
-}
+library(cladoRcpp)
 
 # Plotting scripts
-scriptdir <- np(system.file("extdata/a_scripts", package = "BioGeoBEARS"))
+# scriptdir <- np(system.file("extdata/a_scripts", package = "BioGeoBEARS"))
 #-------------------
 # Read in the trees
 #------------------
@@ -71,120 +67,55 @@ load("biogeography/outputs/pinnipeds-fossil-DECj_9areas_unlikely.Rdata")
 # resDECj1_extant = extant taxa, impossible and unlikely states removed
 load("biogeography/outputs/pinnipeds-extant-DECj_9areas_unlikely.Rdata")
 
-#------------------------
-# Make plots
-#------------------------
+#-----------------------------------------------------------------------
+# Source script with modified BioGeoBEARs plotting function and colours
+#-----------------------------------------------------------------------
+source("biogeography/analyses/modifiedBGBplot.R")
+source("biogeography/analyses/messing-about-with-colours.R")
+
+#--------------------
+# Main results plot
+#--------------------
+pdf("biogeography/outputs/faff.pdf", width = 20, height = 20)
+plot_BioGeoBEARS_mod(resDEC, 
+                     # title info - cex = 0 suppresses title
+                     analysis_titletxt = "DEC all", titlecex = 0, 
+                     # Plot pies at nodes, and size them
+                     plotwhat = "text", statecex = 1, 
+                     # Size and offset of tip names
+                     tipcex = 1, label.offset = 0.2, 
+                     # Plot states at tips, resize tip states and offset of tip labels
+                     tipboxes_TF = TRUE, tip_stateshape = 22, tip_statecex = 3, tip_stateadj = 0.5,
+                     # split splits or not?
+                     plotsplits = FALSE,
+                     # Include null ranges?
+                     include_null_range = TRUE, 
+                     # Input data - tree and geography
+                     tr = tree, tipranges = tipranges,
+                     # x label
+                     xlab = "Time (Ma)", simplify_piecharts = TRUE, 
+                     # colours
+                     colors_list_for_states = colour_list_impossible,
+                     show.tip.label = FALSE)
+
+dev.off()
+
+#--------------------------
+# Make plots for appendices
+#--------------------------
 # All
 pdf(file = "biogeography/outputs/all-pinnipeds-DEC-DECj-plots.pdf", height = 20)
-plot_BioGeoBEARS_results(resDEC, "DEC all", plotwhat = "pie", 
-                         label.offset = 0.45, tipcex = 0.3, statecex = 0.7, splitcex = 0.6, 
-                         titlecex = 0, plotsplits = TRUE, cornercoords_loc = scriptdir, 
-                         include_null_range = TRUE, tr = tree, tipranges = tipranges,
-                         tipboxes_TF = FALSE, xlab = "Time (Ma)")
-
-plot_BioGeoBEARS_results(resDECj, "DECj all", plotwhat = "pie", 
-                         label.offset = 0.45, tipcex = 0.3, statecex = 0.7, splitcex = 0.6, 
-                         titlecex = 0, plotsplits = TRUE, cornercoords_loc = scriptdir, 
-                         include_null_range = TRUE, tr = tree, tipranges = tipranges,
-                         tipboxes_TF = FALSE, xlab = "Time (Ma)")
-
-plot_BioGeoBEARS_results(resDEC1, "DEC unlikely all", plotwhat = "pie", 
-                         label.offset = 0.45, tipcex = 0.3, statecex = 0.7, splitcex = 0.6, 
-                         titlecex = 0, plotsplits = TRUE, cornercoords_loc = scriptdir, 
-                         include_null_range = TRUE, tr = tree, tipranges = tipranges,
-                         tipboxes_TF = FALSE, xlab = "Time (Ma)")
-
-plot_BioGeoBEARS_results(resDECj1, "DECj unlikely all", plotwhat = "pie", 
-                         label.offset = 0.45, tipcex = 0.3, statecex = 0.7, splitcex = 0.6, 
-                         titlecex = 0, plotsplits = TRUE, cornercoords_loc = scriptdir, 
-                         include_null_range = TRUE, tr = tree, tipranges = tipranges,
-                         tipboxes_TF = FALSE, xlab = "Time (Ma)")
 
 dev.off()
 #------------------------
 # Fossil
 pdf(file = "biogeography/outputs/fossil-pinnipeds-DEC-DECj-plots.pdf", height = 20)
-plot_BioGeoBEARS_results(resDEC_fossil, "DEC fossil", plotwhat = "pie", 
-                         label.offset = 0.45, tipcex = 0.3, statecex = 0.7, splitcex = 0.6, 
-                         titlecex = 0, plotsplits = TRUE, cornercoords_loc = scriptdir, 
-                         include_null_range = TRUE, tr = tree, tipranges = tipranges,
-                         tipboxes_TF = FALSE, xlab = "Time (Ma)")
 
-plot_BioGeoBEARS_results(resDECj_fossil, "DECj fossil", plotwhat = "pie", 
-                         label.offset = 0.45, tipcex = 0.3, statecex = 0.7, splitcex = 0.6, 
-                         titlecex = 0, plotsplits = TRUE, cornercoords_loc = scriptdir, 
-                         include_null_range = TRUE, tr = tree, tipranges = tipranges,
-                         tipboxes_TF = FALSE, xlab = "Time (Ma)")
-
-plot_BioGeoBEARS_results(resDEC1_fossil, "DEC unlikely fossil", plotwhat = "pie", 
-                         label.offset = 0.45, tipcex = 0.3, statecex = 0.7, splitcex = 0.6, 
-                         titlecex = 0, plotsplits = TRUE, cornercoords_loc = scriptdir, 
-                         include_null_range = TRUE, tr = tree, tipranges = tipranges,
-                         tipboxes_TF = FALSE, xlab = "Time (Ma)")
-
-plot_BioGeoBEARS_results(resDECj1_fossil, "DECj unlikely fossil", plotwhat = "pie", 
-                         label.offset = 0.45, tipcex = 0.3, statecex = 0.7, splitcex = 0.6, 
-                         titlecex = 0, plotsplits = TRUE, cornercoords_loc = scriptdir, 
-                         include_null_range = TRUE, tr = tree, tipranges = tipranges,
-                         tipboxes_TF = FALSE, xlab = "Time (Ma)")
 
 dev.off()
 
 #------------------------
 # Extant
 pdf(file = "biogeography/outputs/extant-pinnipeds-DEC-DECj-plots.pdf", height = 20)
-plot_BioGeoBEARS_results(resDEC_extant, "DEC extant", plotwhat = "pie", 
-                         label.offset = 0.45, tipcex = 0.3, statecex = 0.7, splitcex = 0.6, 
-                         titlecex = 0, plotsplits = TRUE, cornercoords_loc = scriptdir, 
-                         include_null_range = TRUE, tr = tree, tipranges = tipranges,
-                         tipboxes_TF = FALSE, xlab = "Time (Ma)")
-
-plot_BioGeoBEARS_results(resDECj_extant, "DECj extant", plotwhat = "pie", 
-                         label.offset = 0.45, tipcex = 0.3, statecex = 0.7, splitcex = 0.6, 
-                         titlecex = 0, plotsplits = TRUE, cornercoords_loc = scriptdir, 
-                         include_null_range = TRUE, tr = tree, tipranges = tipranges,
-                         tipboxes_TF = FALSE, xlab = "Time (Ma)")
-
-plot_BioGeoBEARS_results(resDEC1_extant, "DEC unlikely extant", plotwhat = "pie", 
-                         label.offset = 0.45, tipcex = 0.3, statecex = 0.7, splitcex = 0.6, 
-                         titlecex = 0, plotsplits = TRUE, cornercoords_loc = scriptdir, 
-                         include_null_range = TRUE, tr = tree, tipranges = tipranges,
-                         tipboxes_TF = FALSE, xlab = "Time (Ma)")
-
-plot_BioGeoBEARS_results(resDECj1_fextant, "DECj unlikely extant", plotwhat = "pie", 
-                         label.offset = 0.45, tipcex = 0.3, statecex = 0.7, splitcex = 0.6, 
-                         titlecex = 0, plotsplits = TRUE, cornercoords_loc = scriptdir, 
-                         include_null_range = TRUE, tr = tree, tipranges = tipranges,
-                         tipboxes_TF = FALSE, xlab = "Time (Ma)")
 
 dev.off()
-
-
-#
-
-
-
-
-
-#colors_list_for_states = c("#e3342f",
-                           "#f6993f",
-                           "#ffed4a",
-                           "#38c172",
-                           "#4dc0b5",
-                           "#3490dc",
-                           "#6574cd",
-                           "#9561e2",
-                           "#f66d9b")
-
-# options
-results_object, analysis_titletxt = NULL, addl_params = list(), 
-plotwhat = "text", label.offset = NULL, tipcex = 0.8, statecex = 0.7, 
-splitcex = 0.6, titlecex = 0.8, plotsplits = TRUE, plotlegend = FALSE, 
-legend_ncol = NULL, legend_cex = 1, cornercoords_loc = "auto", 
-tr = NULL, tipranges = NULL, if_ties = "takefirst", pie_tip_statecex = 0.7, 
-juststats = FALSE, xlab = "Millions of years ago", root.edge = TRUE, 
-colors_list_for_states = NULL, skiptree = FALSE, show.tip.label = TRUE, 
-tipcol = "black", dej_params_row = NULL, plot_max_age = NULL, 
-skiplabels = FALSE, plot_stratum_lines = TRUE, include_null_range = NULL, 
-plot_null_range = FALSE, simplify_piecharts = FALSE, tipboxes_TF = TRUE, 
-tiplabel_adj = c(0.5), no.margin = FALSE, xlims = NULL, ylims = NULL) 
