38a82e3abfbc0207d22642c7aeb7774e4ac0ec43
Natalie Cooper <ncooper@tcd.ie>
2023-07-03 17:36:32 +0100

    Update node numbers

--- biogeography/analyses/07-Extract-BGB-results-for-plotting.R
+++ biogeography/analyses/07-Extract-BGB-results-for-plotting.R
@@ -102,6 +102,6 @@ cols_byNode <- rangestxt_to_colors(possible_ranges_list_txt,
 #----------------------------------------------
 dd2 <- data.frame(relprobs_matrix_for_internal_states)
 colnames(dd2) <- ranges_list
-dd2$node <- 1:104 # 104 is the number of tips
-dd2$ML <- MLstates[1:104]
-dd2$colour <- cols_byNode[1:104]
+dd2$node <- 106:209 # There are 104 nodes and 105 species
+dd2$ML <- MLstates[106:209] # First node is 106
+dd2$colour <- cols_byNode[106:209]
