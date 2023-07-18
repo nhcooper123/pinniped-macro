1aefdc98fcbe06e08b2a8619d84856c171da7c89
Gustavo Burin Ferreira <arietebio@gmail.com>
2023-07-18 14:40:01 +0100

    updated script for new figures for sensitivity_analysis without sampled ancestors

--- diversification/R/fossilbamm_results_noanc_sensitivity.R
+++ diversification/R/fossilbamm_results_noanc_sensitivity.R
@@ -21,19 +21,19 @@ for(i in 1:length(shifts.prob)){
 
     summary(edata.pinnip)
 
-    png(here::here(paste0("supplemental/figures/diversification/sensitivity_analyses/shifts_", gsub(".", "_", shifts.prob[i], fixed = TRUE), "/phylo_rates_pinnipedia_noanc_speciation_", gsub(".", "_", shifts.prob[i], fixed = TRUE), ".png")), width = 9, height = 9, units = "in", res = 300)
+    png(here::here(paste0("supplemental/figures/diversification/sensitivity_analyses/shifts_", gsub(".", "_", shifts.prob[i], fixed = TRUE), "/phylo_rates_pinnipedia_noanc_speciation_", gsub(".", "_", shifts.prob[i], fixed = TRUE), ".png")), width = 4, height = 5.5, units = "in", res = 300)
     plot.bammdata(edata.pinnip, lwd = 2, legend = TRUE, xlim = c(0, 45), pal = parula(20)[-c(18:20)]);axisPhylo()
     tiplabels(edata.pinnip$tip.label, frame = "none", bg = NULL, adj = c(-0.1, 0.5), cex = 0.75)
     addBAMMshifts(edata.pinnip)
     dev.off()
 
-    png(here::here(paste0("supplemental/figures/diversification/sensitivity_analyses/shifts_", gsub(".", "_", shifts.prob[i], fixed = TRUE), "/phylo_rates_pinnipedia_noanc_extinction_", gsub(".", "_", shifts.prob[i], fixed = TRUE), ".png")), width = 9, height = 9, units = "in", res = 300)
+    png(here::here(paste0("supplemental/figures/diversification/sensitivity_analyses/shifts_", gsub(".", "_", shifts.prob[i], fixed = TRUE), "/phylo_rates_pinnipedia_noanc_extinction_", gsub(".", "_", shifts.prob[i], fixed = TRUE), ".png")), width = 4, height = 5.5, units = "in", res = 300)
     plot.bammdata(edata.pinnip, lwd = 2, legend = TRUE, xlim = c(0, 45), pal = parula(20)[-c(18:20)], spex = "e");axisPhylo()
     tiplabels(edata.pinnip$tip.label, frame = "none", bg = NULL, adj = c(-0.1, 0.5), cex = 0.75)
     addBAMMshifts(edata.pinnip)
     dev.off()
 
-    png(here::here(paste0("supplemental/figures/diversification/sensitivity_analyses/shifts_", gsub(".", "_", shifts.prob[i], fixed = TRUE), "/phylo_rates_pinnipedia_noanc_netdiv_", gsub(".", "_", shifts.prob[i], fixed = TRUE), ".png")), width = 9, height = 9, units = "in", res = 300)
+    png(here::here(paste0("supplemental/figures/diversification/sensitivity_analyses/shifts_", gsub(".", "_", shifts.prob[i], fixed = TRUE), "/phylo_rates_pinnipedia_noanc_netdiv_", gsub(".", "_", shifts.prob[i], fixed = TRUE), ".png")), width = 4, height = 5.5, units = "in", res = 300)
     plot.bammdata(edata.pinnip, lwd = 2, legend = TRUE, xlim = c(0, 45), pal = parula(20)[-c(18:20)], spex = "netdiv");axisPhylo()
     tiplabels(edata.pinnip$tip.label, frame = "none", bg = NULL, adj = c(-0.1, 0.5), cex = 0.75)
     addBAMMshifts(edata.pinnip)
